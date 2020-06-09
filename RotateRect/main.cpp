947d1de36afa03e04df53132c78ea96447586be6
Gosia Szczepańska <gosia.szczepanska1@interia.pl>
2020-06-10 01:00:20 +0200

    Implement constant rotation speed with random frame delay

--- RotateRect/main.cpp
+++ RotateRect/main.cpp
@@ -5,46 +5,74 @@
 
 typedef boost::random::mt19937 gen_type;
 typedef boost::random::beta_distribution<> beta_dist_type;
-typedef boost::random::variate_generator<gen_type&, beta_dist_type> variate_gen_type;
+typedef boost::random::uniform_real_distribution<> uniform_dist_type;
+typedef boost::random::variate_generator<gen_type&, beta_dist_type> beta_gen_type;
+typedef boost::random::variate_generator<gen_type&, uniform_dist_type> uniform_gen_type;
 
-double get_random()
+// values for median = 0.33 for beta dist
+//const float alpha = 1.71347194248032f;
+//const float beta = 5.f;
+
+//const float alpha = 1.5f;
+//const float beta = 4.29304676159030f;
+
+const float alpha = 0.797341459119764;
+const float beta = 2;
+
+const int max = 100;
+const int min = 10;
+// Hz
+double angular_speed;
+gen_type random_generator(time(NULL));
+uniform_dist_type uniform_dist(0.2, 2);
+uniform_gen_type random_uniform_generator(random_generator, uniform_dist);
+beta_dist_type beta_dist(alpha, beta);
+beta_gen_type random_beta_generator(random_generator, beta_dist);
+
+double get_random_beta()
 {
-    // values for median = 0.33 for beta dist
-    float alpha = 1.71347194248032f;
-    float beta = 5.f;
-    int max = 100;
-    int min = 10;
-
-    gen_type random_generator(time(NULL));
-    beta_dist_type beta_dist(alpha, beta);
-    variate_gen_type random_beta_generator(random_generator, beta_dist);
     return (random_beta_generator() * (max - min)) + min;
+}
+
+double get_random_uniform()
+{
+    return random_uniform_generator();
 }
 
 int main()
 {
+    angular_speed = get_random_uniform() * 360;
+
     sf::RenderWindow window(sf::VideoMode(300, 300), "");
     sf::RectangleShape shape(sf::Vector2f(50, 100));
     shape.setFillColor(sf::Color::Green);
     shape.setOrigin(25, 50);
     shape.setPosition(150, 150);
+    sf::Clock clock;
 
     while (window.isOpen())
     {
+        float delay = get_random_beta();
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
-        shape.rotate(1);
+
+        while (clock.getElapsedTime().asSeconds() < delay/1000)
+        {
+        }
+
+        float deg = clock.getElapsedTime().asSeconds() * angular_speed;
+        //std::cout << "delay: " << delay << " rotate: "<< deg << " speed: "<< deg/delay << " ang_speed: " << angular_speed << " ratio: " << (deg/delay)/angular_speed << std::endl;
+        shape.rotate(deg);
         window.draw(shape);
         window.display();
-        get_random();
+
+        clock.restart();
     }
 
     return 0;
 }
-
